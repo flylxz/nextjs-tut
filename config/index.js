f46d8ab8dfb7a498694254622d1bc7780f9e2f7e
flylxz <flylxz@gmail.com>
2021-01-31 22:36:10 +0200

    vercel fix

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
   ? 'http://localhost:3000'
-  : `https://nextjs-tut-19qmn7xc9.vercel.app`;
+  : `https://nextjs-tut-lac.vercel.app/`;
