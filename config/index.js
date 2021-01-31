e36e26267053c9c01ceeab2df9d8f7af61b8cb58
flylxz <flylxz@gmail.com>
2021-01-31 21:58:47 +0200

    small fix

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev
-  ? 'http://localhost:3000'
-  : process.env.API_URL || VERCEL_URL;
+export const server = dev ? 'http://localhost:3000' : VERCEL_URL;
